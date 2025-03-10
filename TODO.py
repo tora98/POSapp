18f8cb849f393ca948c19fbde0ddf1a1aa33991b
MilerB <milerbayangan@gmail.com>
2025-03-10 17:33:15 +0800

    Added some ideas

--- TODO.py
+++ TODO.py
@@ -1,4 +1,8 @@
 #using grid is cofusing, use pack or place instead to make the sales work
+    #try to replace the 'place()' layout to make use of 'Notebook' widget
+#create the logic for the adding of products
+#Create a GUI for adding products
+#Make the funtion logics work
 #Connect the database to the GUI
 #Sales View in a table when a product is sold
 #Create a Log In System and GUI
