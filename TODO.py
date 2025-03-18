1e55adb2e367ec743aaa80a0d689429ca1e75a7e
Miler Bayangan <milerbayangan@gmail.com>
2025-03-18 14:02:02 +0800

    Delete TODO.py
    renamed TODO into todo

--- TODO.py
+++
@@ -1,8 +0,0 @@
-#Replace database (added blank users with bad code)
-#Login validtion
-#Delete and Update logic on product list
-#Show sale items on treeview when sold
-#Adding scroll bars into tree views
-#Try to implement a search function in the table
-#Try to implement a weekly and monthly sales report
-#...loading brain...
