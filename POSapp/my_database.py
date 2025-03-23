3b46eeaa76f56efedde098b3309380d717e78667
MilerB <milerbayangan@gmail.com>
2025-03-23 12:03:29 +0800

    RE:Added file for creating database

---
+++ POSapp/my_database.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+conn = sqlite3.connect("posdb.db")
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS employees (
+        employee_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        complete_name TEXT NOT NULL,
+        password TEXT NOT NULL
+    )
+''')
+conn.commit()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS products (
+        product_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        product_name TEXT NOT NULL,
+        manufacturer TEXT NOT NULL,
+        packaging_units TEXT NOT NULL,
+        price_per_unit REAL NOT NULL
+    )
+''')
+conn.commit()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS sales (
+        sale_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT NOT NULL,
+        product_id INTEGER NOT NULL,
+        quantity INTEGER NOT NULL,
+        total_price REAL NOT NULL,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY (product_id) REFERENCES products(product_id),
+        FOREIGN KEY (user_id) REFERENCES employees(employee_id)
+    )
+''')
+conn.commit()
+
+cursor.execute('''INSERT INTO employees (username, complete_name, password) VALUES ('Admin', 'Administrator', 'admin')''')
+conn.commit()
+conn.close()
