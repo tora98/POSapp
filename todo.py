c09a8c21f3083542886ad189b73bb896f8b3bdd5
MilerB <milerbayangan@gmail.com>
2025-04-21 15:40:00 +0800

    modified

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
 # Add bindings to entry and buttons
 # Update Logic for UserList
-# Logic to disable Product instead of deleting
 # Logic to accept values when a list item is selectd or clicked
+# Add a feature for generating excel reports
